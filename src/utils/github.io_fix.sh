6be87524fae4e501559809a9a3ec0e3199c53f29
Pranav Ballaney <ballaneypranav@gmail.com>
2020-08-22 09:05:37 +0530

    Switch to Copier template

--- src/utils/github.io_fix.sh
+++
@@ -1,2 +0,0 @@
-{ echo -n '- urlPrefix = "/{{ cookiecutter.github_repository }}/"\n//- '; cat src/templates/mixins.pug; } >src/templates/mixins.new.pug
-mv src/templates/mixins.new.pug src/templates/mixins.pug
